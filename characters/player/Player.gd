e8daeecad4671b43ea1d14327b5c23343e5b7299
BesterCoder <bestercoder@protonmail.com>
2020-11-18 12:31:25 +0200

    Player's halo now stays on ground

--- characters/player/Player.gd
+++ characters/player/Player.gd
@@ -6,9 +6,10 @@ const JUMPFORCE = -1000
 # How much does the player body tilt/rotate when moving
 const TILT_DEGREE = 5.0
 
-enum move_dir {MOVE_LEFT, MOVE_RIGHT, MOVE_NONE}
+enum move_dir {MOVE_LEFT = -1, MOVE_RIGHT = 1, MOVE_NONE = 0}
 
 var velocity = Vector2(0, 0)
+var player_halo = null
 
 func _tilt_player(dir):
 	match dir:
@@ -19,13 +20,31 @@ func _tilt_player(dir):
 		move_dir.MOVE_NONE:
 			$Body.rotation_degrees = 0
 
+	$halo_floor.position.x = $Body.texture.get_width() * dir
+
+func _halo_location():
+	if is_on_floor():
+		player_halo.position = self.position
+		# +39 seems to look the best
+		player_halo.position.y += 39
+		return
+
+	if not $halo_floor.is_colliding():
+		return
+
+	var col_point =  $halo_floor.get_collision_point()
+	player_halo.position = col_point
+	# -25 seems to look the best in the game
+	player_halo.position.y -= 25
+	player_halo.position.x = self.position.x
 
 func _ready():
-	pass # Replace with function body.
-
+	player_halo = get_tree().get_root().find_node("PlayerGroundHalo", true, false)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
+	_halo_location()
+
 	if Input.is_action_pressed("move_right"):
 		_tilt_player(move_dir.MOVE_RIGHT)
 		velocity.x = SPEED
@@ -36,7 +55,6 @@ func _process(_delta):
 		_tilt_player(move_dir.MOVE_NONE)
 
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		# TODO: Leave the GroundHalo on ground when jumping
 		velocity.y = JUMPFORCE
 
 	velocity.y += GRAVITY
