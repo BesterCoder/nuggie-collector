0799bea9e36d64b818958598c1a15473a1367a90
BesterCoder <bestercoder@protonmail.com>
2020-11-23 10:24:46 +0200

    Added timers to weapons to limit fire rate

--- characters/weapons/rifle/Rifle.gd
+++ characters/weapons/rifle/Rifle.gd
@@ -2,8 +2,23 @@ extends Sprite
 
 export var weapon_bullet: PackedScene
 
+var shoot_timer = null
+var shoot_ready = true
+
+func _ready():
+	shoot_timer = Timer.new()
+	add_child(shoot_timer)
+	shoot_timer.wait_time = 0.2
+	shoot_timer.connect("timeout", self, "_on_shoot_ready")
+
+func _on_shoot_ready():
+	shoot_ready = true
 
 func shoot():
+	if not shoot_ready:
+		return
+	shoot_ready = false
+
 	var point_position = $BulletPoint.global_position
 	# TODO: cleaner way to change the y position when weapon is flipped
 	if self.flip_v:
@@ -16,3 +31,4 @@ func shoot():
 	new_bullet.global_position = point_position
 
 	get_tree().current_scene.add_child(new_bullet)
+	shoot_timer.start()
