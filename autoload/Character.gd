04b2dc7991c1398694a9a0cd71760f18cab6af61
BesterCoder <bestercoder@protonmail.com>
2020-12-10 22:41:14 +0200

    Added initial funcitonality to the character menu

---
+++ autoload/Character.gd
@@ -0,0 +1,56 @@
+extends Node
+
+
+var __hp: int = 100
+var __damage: int = 1
+const __HEALTH_STEP: int = 10
+const __DAMAGE_STEP: int = 1
+var __health_points: int = 0
+var __damage_points: int = 0
+var __points_left: int = 5
+
+
+func get_hp() -> int:
+	return __hp + __HEALTH_STEP * __health_points
+
+
+func get_damage() -> int:
+	return __damage + __DAMAGE_STEP * __damage_points
+
+
+func get_health_points() -> int:
+	return __health_points
+
+
+func get_damage_points() -> int:
+	return __damage_points
+
+
+func get_points_left() -> int:
+	return __points_left
+
+
+func add_points(amount: int = 1) -> void:
+	if amount < 0 && __points_left <= 0:
+		return
+	__points_left += amount
+
+
+func add_damage_points(amount: int = 1) -> void:
+	if amount < 0 && __damage_points <= 0:
+		return
+	if amount > 0 && __points_left <= 0:
+		return
+
+	__points_left -= amount
+	__damage_points += amount
+
+
+func add_health_points(amount: int = 1) -> void:
+	if amount < 0 && __health_points <= 0:
+		return
+	if amount > 0 && __points_left <= 0:
+		return
+
+	__points_left -= amount
+	__health_points += amount
---
+++ menus/character/CharacterMenu.gd
@@ -0,0 +1,31 @@
+extends Control
+
+
+func _ready():
+	_set_values()
+
+
+func _set_values():
+	$Info/HpPoints.text = String(Character.get_hp())
+	$Info/DamagePoints.text = String(Character.get_damage())
+	$PointsLeft/Points.text = String(Character.get_points_left())
+	$HealthPoints/Points.text = String(Character.get_health_points())
+	$DamagePoints/Points.text = String(Character.get_damage_points())
+
+
+
+func _on_health_plus_pressed():
+	Character.add_health_points(1)
+	_set_values()
+
+func _on_health_minus_pressed():
+	Character.add_health_points(-1)
+	_set_values()
+
+func _on_damage_plus_pressed():
+	Character.add_damage_points(1)
+	_set_values()
+
+func _on_damage_minus_pressed():
+	Character.add_damage_points(-1)
+	_set_values()
