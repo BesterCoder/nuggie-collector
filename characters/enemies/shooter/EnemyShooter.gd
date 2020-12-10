7390ea6100bc585dc66a602da5c120d5eac9017f
BesterCoder <bestercoder@protonmail.com>
2020-12-10 14:27:53 +0200

    Enemy shooter now supports all weapon types
    Pistol, rifle and shotgun can now be selected
    with the scene editor

--- characters/enemies/shooter/EnemyShooter.gd
+++ characters/enemies/shooter/EnemyShooter.gd
@@ -1,6 +1,15 @@
 extends EnemyBase
 
+enum {
+	WEAPON_PISTOL = 1,
+	WEAPON_RIFLE = 2,
+	WEAPON_SHOTGUN = 3
+}
+
+export var weapon_type = 1
+
 var shoot_target = null
+var weapon = null
 
 func _weapon_pos():
 	if shoot_target == null:
@@ -12,16 +21,32 @@ func _weapon_pos():
 		direction = -1
 
 	if direction < 0:
-		$Pistol.flip_v = true
+		weapon.flip_v = true
 	else:
-		$Pistol.flip_v = false
+		weapon.flip_v = false
 
 	# Set weapon position
 	var offset = 100 * direction
-	$Pistol.position.x = $Body.texture.get_width() * direction - offset
+	weapon.position.x = $Body.texture.get_width() * direction - offset
 
 func _child_ready():
-	pass
+	$Pistol.visible = false
+	$Rifle.visible = false
+	$Shotgun.visible = false
+
+	match weapon_type:
+		WEAPON_PISTOL:
+			weapon = $Pistol
+		WEAPON_RIFLE:
+			weapon = $Rifle
+		WEAPON_SHOTGUN:
+			weapon = $Shotgun
+		_:
+			push_error("EnemyShooter.gd: Invalid weapon_type: %d" % drop_type)
+
+	weapon.visible = true
+
+
 
 func _child_change_direction():
 	pass
@@ -29,11 +54,11 @@ func _child_change_direction():
 func _child_physics_process(_delta):
 	if shoot_target != null:
 		_weapon_pos()
-		$Pistol.look_at(shoot_target.global_position)
-		$Pistol.shoot(shoot_target)
+		weapon.look_at(shoot_target.global_position)
+		weapon.shoot(shoot_target)
 
 func _on_player_body_entered(body):
-	$Pistol.look_at(body.global_position)
+	weapon.look_at(body.global_position)
 	shoot_target = body
 	speed = 0
 
@@ -41,7 +66,7 @@ func _on_player_body_entered(body):
 func _on_player_body_exited(_body):
 	shoot_target = null
 	speed = 50
-	if $Pistol.flip_v:
-		$Pistol.rotation = 180 - 45
+	if weapon.flip_v:
+		weapon.rotation = 180 - 45
 	else:
-		$Pistol.rotation = 0.0
+		weapon.rotation = 0.0
