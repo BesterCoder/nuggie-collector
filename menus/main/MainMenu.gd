c6d4b0037a48250aa2292881f5444dec523d04de
BesterCoder <bestercoder@protonmail.com>
2020-12-10 22:40:59 +0200

    Connect character menu to main menu

--- menus/main/MainMenu.gd
+++ menus/main/MainMenu.gd
@@ -3,3 +3,7 @@ extends Control
 
 func _on_play_pressed():
 	SceneLoader.load_scene("res://levels/level_001.tscn")
+
+
+func _on_character_pressed():
+	SceneLoader.load_scene("res://menus/character/CharacterMenu.tscn")
