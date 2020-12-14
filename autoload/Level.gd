b5530a14a219c8cb40abe33d4a920859b81a088d
BesterCoder <bestercoder@protonmail.com>
2020-12-14 15:21:36 +0200

    Initial level 7 layout

--- autoload/Level.gd
+++ autoload/Level.gd
@@ -8,7 +8,8 @@ var __level_list = [
 	["res://levels/level_003.tscn", false],
 	["res://levels/level_004.tscn", false],
 	["res://levels/level_005.tscn", false],
-	["res://levels/level_006.tscn", false]
+	["res://levels/level_006.tscn", false],
+	["res://levels/level_007.tscn", false]
 ]
 
 var __level_count = len(__level_list)
